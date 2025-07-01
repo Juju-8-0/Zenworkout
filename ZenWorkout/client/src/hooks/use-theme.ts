8936ae8c9395778936e9d50a82340c00fc083573
Juju-8-0 <cervantesjuliand@gmail.com>
2025-06-30 18:49:45 -0700

    Delete ZenWorkout directory
    not needed

--- ZenWorkout/client/src/hooks/use-theme.ts
+++
@@ -1,5 +0,0 @@
-import { useContext } from "react";
-import { ThemeProvider } from "@/components/theme-provider";
-
-// This is re-exported from theme-provider for convenience
-export { useTheme } from "@/components/theme-provider";
